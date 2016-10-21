997365ca442de660259fe50815780db6880d0060
liuzhiyong14 <liuzhiyong14@wanda.cn>
2016-10-21 16:18:32 +0800

    代码优化

--- src/test/java/com/hessian/client/HessianTest.java
+++ src/test/java/com/hessian/client/HessianTest.java
@@ -1,6 +1,5 @@
 package com.hessian.client;
 
-
 import java.net.MalformedURLException;
 
 import org.junit.Test;
@@ -17,50 +16,49 @@ import com.hessian.service.common.RequestParam;
 import com.hessian.service.common.User;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"classpath*:spring.xml"})
+@ContextConfiguration(locations = { "classpath*:spring.xml" })
 public class HessianTest {
-	
+
 	@Autowired
 	IUserService userService;
 
 	@Test
 	public void hessianTest() {
 		String result = userService.hello("china");
-		String json =  JSONObject.toJSONString(userService.getById(3));
-		
+		String json = JSONObject.toJSONString(userService.getById(3));
+
 		RequestParam param = new RequestParam();
 		param.setName("老四");
+		param.setCard(123456);
 		User user = userService.getUser(param);
-		
-		System.out.println(String.format("result is [%s]",result));
-		System.out.println(String.format("json is [%s]",json));
+
+		System.out.println(String.format("result is [%s]", result));
+		System.out.println(String.format("json is [%s]", json));
 		System.out.println(String.format("user is [%s]",JSONObject.toJSONString(user)));
-		
+
 	}
-	
+
 	@Test
-	public void servletTest(){
+	public void servletTest() {
 		String url = "http://10.15.135.184:8080/hello";
-		
+
 		HessianProxyFactory factory = new HessianProxyFactory();
 		try {
 			IServlet servlet = (IServlet) factory.create(IServlet.class, url);
 			String result = servlet.hello("china");
-			
-			String json =  JSONObject.toJSONString(servlet.getById(3));
-			
+
+			String json = JSONObject.toJSONString(servlet.getById(3));
+
 			RequestParam param = new RequestParam();
 			param.setName("老四");
 			User user = servlet.getUser(param);
-			
-			
-			System.out.println(String.format("result is [%s]",result));
-			System.out.println(String.format("json is [%s]",json));
+
+			System.out.println(String.format("result is [%s]", result));
+			System.out.println(String.format("json is [%s]", json));
 			System.out.println(String.format("user is [%s]",JSONObject.toJSONString(user)));
-			
+
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
 		}
-		
 	}
 }
